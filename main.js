795fa77ae0e23d49169212121c3032fdf7206785
zezo386 <ziad.elhusiny@gmail.com>
2026-08-07 18:03:26 +0300

    Added control panel to change octave and waveform

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ Tone.start();
 const synth = new Tone.PolySynth(Tone.Synth).toDestination();
 
 let pressedKeys = new Set()
+let Octave = 4;
 const codeToKey = {
     "s":"C",
     "e":"C#",
@@ -18,26 +19,42 @@ const codeToKey = {
 }
 
 function playNote(Note){
-    synth.triggerAttack(`${Note}4`);
+    synth.triggerAttack(`${Note}${Octave}`);
     console.log(pressedKeys)
 }
 
 function finishNote(Note){
-    synth.triggerRelease(`${Note}4`,"+0.25");
+    synth.triggerRelease(`${Note}${Octave}`,"+0.25");
     
 }
 
 document.onkeydown = function (e){
+    e.preventDefault();
     let key = e.key;
+    console.log(key);
     if ("sedrfgyhujik".includes(key.toLowerCase()) && !pressedKeys.has(key)){
         let note = codeToKey[key];
         pressedKeys.add(key);
         playNote(note);
         document.getElementById(note).style.backgroundColor = `rgb(237, 18, 91)`;
     }
+    if (key == "ArrowUp" && Octave + 2 <= 10 ){
+        Octave += 2;
+    }
+    if (key == "ArrowDown" && Octave - 2 >= 1){
+        Octave -= 2;
+    }
+    if (key == "ArrowRight" && Octave + 2 <= 10){
+        Octave += 1;
+    }
+    if (key == "ArrowLeft" && Octave - 1 >= 1){
+        Octave -= 1;
+    }
+    document.getElementById("Octave").innerText = Octave;
 }
 
 document.onkeyup = function (e){
+    e.preventDefault();
     let key = e.key;
     if ("sedrfgyhujik".includes(key.toLowerCase()) && pressedKeys.has(key)){
         let note = codeToKey[key];
@@ -47,3 +64,8 @@ document.onkeyup = function (e){
         document.getElementById(note).style.backgroundColor = note.includes("#") ? `black` : `white`;
     }
 }
+
+document.getElementById("waveFormSelect").addEventListener("change",(e) => {
+    e.preventDefault();
+    synth.set({oscillator:{type: e.target.value}});
+})
